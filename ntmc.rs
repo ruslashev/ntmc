63291ca31b98a5f8969ddaf97d751500bf2ed561
Ruslan Akbashev <ruslashev@gmail.com>
2023-06-17 15:06:11 +0400

    parse args

--- ntmc.rs
+++ ntmc.rs
@@ -1,3 +1,73 @@
+#![allow(clippy::uninlined_format_args)]
+
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+
+    println!("{:?}", args);
+}
+
+#[derive(Debug)]
+struct Args {
+    input: String,
+    output: Option<String>,
+}
+
+fn parse_args() -> Args {
+    let args = std::env::args().collect::<Vec<String>>();
+    let args = args.iter().map(String::as_str).collect::<Vec<&str>>();
+    let [_progname, tail @ ..] = args.as_slice() else {
+        usage_err("empty args list");
+    };
+
+    let mut it = tail;
+    let mut input = None;
+    let mut output = None;
+
+    loop {
+        match it {
+            ["-h" | "--help", ..] => usage(0),
+            ["-v" | "--version", ..] => version(),
+            ["-o" | "--output", filename, rest @ ..] => {
+                output = Some((*filename).to_string());
+                it = rest;
+            }
+            [filename, rest @ ..] => {
+                match input {
+                    None => input = Some((*filename).to_string()),
+                    Some(_) => usage_err("multiple input files provided"),
+                }
+                it = rest;
+            }
+            _ => break,
+        }
+    }
+
+    let input = input.unwrap_or_else(|| usage_err("no input filename given"));
+
+    Args { input, output }
+}
+
+fn usage_err(msg: &'static str) -> ! {
+    println!("Error: {}", msg);
+    usage(1);
+}
+
+fn usage(ret: i8) -> ! {
+    let text = "\
+Usage: ntmc [options] <file>
+Options:
+    -o, --output <file>   Store output in <file>
+    -h, --help            Display help
+    -v, --version         Display version";
+
+    println!("{}", text);
+
+    exit(ret.into());
+}
+
+fn version() -> ! {
+    println!("ntmc {}", env!("CARGO_PKG_VERSION"));
+    exit(0);
 }
